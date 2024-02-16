bdf4df75380f6c76117fa4ae17c16db41641dc3a
marciliodevjava <marciliodevjava@gmail.com>
2024-02-15 21:12:00 -0300

    Update message

--- enuns/message.py
+++ enuns/message.py
@@ -2,8 +2,10 @@ from enuns import Enum
 
 
 class MessageProduto(Enum):
+    PRODUTO_ATUALIZADO = 'Produto atualizado com sucesso!'
+    PRODUTO_NAO_ENCONTRADO = 'O produto {} não foi encontrado.'
     PRODUTO_JA_EXISTE = 'O produto {} já existe na base de dados.'
     PRODUTO_CRIADO_COM_SUCESSO = 'O {} foi criado com sucesso!'
-    PRODUTO_PARSER_VALOR = 'O campo "valor" não foi enviado.'
-    PRODUTO_PARSER_DESCRICAO = 'O campo "descricao" não foi enviado.'
-    PRODUTO_PARSER_NOME = 'O campo "nome" não foi enviado.'
+    PRODUTO_PARSER_VALOR = "O campo 'valor' não foi enviado."
+    PRODUTO_PARSER_DESCRICAO = "O campo 'descricao' não foi enviado."
+    PRODUTO_PARSER_NOME = "O campo 'nome' não foi enviado."
