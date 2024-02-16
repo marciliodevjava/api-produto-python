cb6ee49dd63a7dc102462a5292969137cecb5b1f
marciliodevjava <marciliodevjava@gmail.com>
2024-02-16 06:25:32 -0300

    Update app

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_restful import Api
 
 from config.config import SQLALCHEMY_DATABASE_URI
 from repository.produto_model import db
+from resource.login_resource import Login, LoginCadastro
+from resource.produto_resource import Produto
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
@@ -11,12 +13,17 @@ db.init_app(app)
 
 api = Api(app)
 
-from resource.produto_resource import Produto
-
+# Endpoint de Produto
 api.add_resource(Produto, '/produto', endpoint='post')
 api.add_resource(Produto, '/produto/<string:nome>', endpoint='get')
 api.add_resource(Produto, '/produto/<int:id>', endpoint='put')
 api.add_resource(Produto, '/produto/<int:id>', endpoint='delete')
+
+# Endpoint de Login
+api.add_resource(Login, '/login')
+
+# Endpoint de cadastro de login
+api.add_resource(LoginCadastro, '/cadastro')
 
 if __name__ == '__main__':
     app.run(debug=False)
