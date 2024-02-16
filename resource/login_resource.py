e7734ebe13001aac1bfe895aa4e90886d20b4230
marciliodevjava <marciliodevjava@gmail.com>
2024-02-16 06:25:40 -0300

    Update login_resource.py

---
+++ resource/login_resource.py
@@ -0,0 +1,15 @@
+from flask_restful import Resource
+
+
+class Login(Resource):
+
+    def post(self):
+        pass
+
+
+class LoginCadastro(Resource):
+    def post(self):
+        pass
+
+    def delete(self):
+        pass
