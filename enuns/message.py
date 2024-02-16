55da0fe05e7f3b1ceb4ed0736528f826b8323d87
marciliodevjava <marciliodevjava@gmail.com>
2024-02-15 21:47:10 -0300

    Update message

--- enuns/message.py
+++ enuns/message.py
@@ -2,7 +2,9 @@ from enuns import Enum
 
 
 class MessageProduto(Enum):
-    PRODUTO_ERRO_AO_SALVAR = 'Erro ao salvar produto'
+    PRODUTO_NAO_FOI_DELETADO = 'Ocorreu um erro ao deletar o produto.'
+    PRODUTO_DELETADO_COM_SUCESSO = 'Produto deletado com Sucesso.'
+    PRODUTO_ERRO_AO_SALVAR = 'Erro ao salvar produto.'
     PRODUTO_ATUALIZADO = 'Produto atualizado com sucesso!'
     PRODUTO_NAO_ENCONTRADO = 'O produto {} não foi encontrado.'
     PRODUTO_JA_EXISTE = 'O produto {} já existe na base de dados.'
