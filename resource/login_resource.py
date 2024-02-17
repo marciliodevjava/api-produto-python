a1535ca6541d01f717d813367b65fa4b62dfc89f
marciliodevjava <marciliodevjava@gmail.com>
2024-02-17 19:53:29 -0300

    Update login_resource.py

--- resource/login_resource.py
+++ resource/login_resource.py
@@ -1,9 +1,41 @@
+from secrets import compare_digest
+
+from flask_jwt_extended import create_access_token
 from flask_restful import Resource, reqparse
+
 from enuns.message import MessageLogin
 from repository.usuario_model import UsuarioModel
 
 
 class Login(Resource):
+    def __init__(self):
+        self.__parcer = reqparse.RequestParser()
+        self.__parcer.add_argument('login', type=str, required=True, help=MessageLogin.LOGIN_REQUERIDO)
+        self.__parcer.add_argument('senha', type=str, required=True, help=MessageLogin.SENHA_REQUIRIDO)
+
+    def post(self):
+        dados = self.__parcer.parse_args()
+        user = UsuarioModel.buscar(dados['login'])
+        if user:
+            if user and compare_digest(user.senha, dados['senha']):
+                access_token = create_access_token(identity=user.id)
+                return {
+                    'message': MessageLogin.USUARIO_LOGADO_COM_SUCESSO,
+                    'acces_token': access_token,
+                    'nome': user.nome,
+                    'login': user.login
+                }, 200
+            else:
+                return {
+                    'message': MessageLogin.USUARIO_SENHA_INCORRETA,
+                    'login': dados['login']
+                }, 500
+        return {
+            'message': MessageLogin.USUARIO_E_SENHA_INCORRETA
+        }, 500
+
+
+class LoginCadastro(Resource):
     def __init__(self):
         self.__parcer = reqparse.RequestParser()
         self.__parcer.add_argument('nome', type=str, required=False)
@@ -29,12 +61,6 @@ class Login(Resource):
             return {
                 'message': MessageLogin.OCORREU_UM_ERRO_AO_SALVAR_USUARIO
             }, 500
-
-
-
-class LoginCadastro(Resource):
-    def post(self):
-        pass
 
     def delete(self):
         pass
