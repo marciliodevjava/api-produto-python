4e3470c946588128deecdf755b1b4e24fca00783
marciliodevjava <marciliodevjava@gmail.com>
2024-02-17 16:27:27 -0300

    Update message.py

--- enuns/message.py
+++ enuns/message.py
@@ -12,3 +12,8 @@ class MessageProduto(Enum):
     PRODUTO_PARSER_VALOR = "O campo 'valor' não foi enviado."
     PRODUTO_PARSER_DESCRICAO = "O campo 'descricao' não foi enviado."
     PRODUTO_PARSER_NOME = "O campo 'nome' não foi enviado."
+
+class MessageLogin(Enum):
+    USUARIO_JA_EXISTE = 'O Usuário {} já existe, utilize outro nome.'
+    SENHA_REQUIRIDO = 'O campo "senha" precisa ser enviado.'
+    LOGIN_REQUERIDO = 'O campo "login" precisa ser enviado.'
