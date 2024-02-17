2d2d1067ee7740628d620033364cd0a13c653f6d
marciliodevjava <marciliodevjava@gmail.com>
2024-02-17 16:27:16 -0300

    Create usuario_model.py

---
+++ repository/usuario_model.py
@@ -0,0 +1,22 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class UsuarioModel(db.Model):
+    __tablename__ = 'usuario'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(150), nullable=False)
+    login = db.Column(db.String(50), nullable=False)
+    senha = db.Column(db.String(50), nullable=False)
+
+    def __init__(self, nome, login, sneha, **dados):
+        self.nome = nome
+        self.login = login
+        self.senha = sneha
+
+    def json(self):
+        return {
+            'nome': self.nome,
+            'login': self.login
+        }
