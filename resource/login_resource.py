ba7cc45db4d4a31392facbb52521070db16126e5
marciliodevjava <marciliodevjava@gmail.com>
2024-02-17 16:27:36 -0300

    Update login_resource.py

--- resource/login_resource.py
+++ resource/login_resource.py
@@ -1,10 +1,23 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+from enuns.message import MessageLogin
+from repository.usuario_model import UsuarioModel
 
 
 class Login(Resource):
+    def __init__(self):
+        self.__parcer = reqparse.RequestParser()
+        self.__parcer.add_argument('nome', type=str, required=False)
+        self.__parcer.add_argument('login', type=str, required=True, help=MessageLogin.LOGIN_REQUERIDO)
+        self.__parcer.add_argument('senha', type=str, required=True, help=MessageLogin.SENHA_REQUIRIDO)
 
     def post(self):
-        pass
+        dados = self.__parcer.parse_args()
+        login = UsuarioModel.query.filter_by(login=dados['login']).first()
+        if login:
+            return {
+                'message': MessageLogin.USUARIO_JA_EXISTE.format(dados['login']),
+                'usuario': dados.get('login')
+            }, 500
 
 
 class LoginCadastro(Resource):
