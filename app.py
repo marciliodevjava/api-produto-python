18cbb12c17349d50142c59c0375a3731bc4384c1
marciliodevjava <marciliodevjava@gmail.com>
2024-02-17 19:53:37 -0300

    Update app

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask
 from flask_restful import Api
-from flask_jwt_extended import JWTManager
-
+from extensao import jwt, db
 from config.config import SQLALCHEMY_DATABASE_URI
-from repository.produto_model import db
 from resource.login_resource import Login, LoginCadastro
 from resource.produto_resource import Produto
 
@@ -11,22 +9,22 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = '123'
-jwt = JWTManager(app)
+
+jwt.init_app(app)
 db.init_app(app)
 
 api = Api(app)
 
-# Endpoint de Produto
+# Adicione seus recursos aqui
 api.add_resource(Produto, '/produto', endpoint='post')
 api.add_resource(Produto, '/produto/<string:nome>', endpoint='get')
 api.add_resource(Produto, '/produto/<int:id>', endpoint='put')
 api.add_resource(Produto, '/produto/<int:id>', endpoint='delete')
 
-# Endpoint de Login
 api.add_resource(Login, '/login')
-
-# Endpoint de cadastro de login
 api.add_resource(LoginCadastro, '/cadastro')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=False)
