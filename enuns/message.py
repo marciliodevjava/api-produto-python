94c0c530ee381968fc24e3cc858f7363b4c650ec
marciliodevjava <marciliodevjava@gmail.com>
2024-02-19 20:49:56 -0300

    Update message

--- enuns/message.py
+++ enuns/message.py
@@ -2,6 +2,7 @@ from enuns import Enum
 
 
 class MessageProduto(Enum):
+    PRODUTO_PARSER_PRECO = 'É necessario enviar o preço do produto'
     PRODUTO_NAO_FOI_DELETADO = 'Ocorreu um erro ao deletar o produto.'
     PRODUTO_DELETADO_COM_SUCESSO = 'Produto deletado com Sucesso.'
     PRODUTO_ERRO_AO_SALVAR = 'Erro ao salvar produto.'
@@ -13,6 +14,7 @@ class MessageProduto(Enum):
     PRODUTO_PARSER_DESCRICAO = "O campo 'descricao' não foi enviado."
     PRODUTO_PARSER_NOME = "O campo 'nome' não foi enviado."
 
+
 class MessageLogin(Enum):
     USUARIO_E_SENHA_INCORRETA = 'Usuário e senha inválidos.'
     USUARIO_SENHA_INCORRETA = 'Senha incorreta, tente novamente.'
@@ -22,7 +24,12 @@ class MessageLogin(Enum):
     USUARIO_JA_EXISTE = 'O Usuário {} já existe, utilize outro nome.'
     SENHA_REQUIRIDO = 'O campo "senha" precisa ser enviado.'
     LOGIN_REQUERIDO = 'O campo "login" precisa ser enviado.'
-    
+
+
 class MessageToken(Enum):
     TOKEN_EXPIRADO = 'Token expirado, renove o seu token e tente novamente'
     TOKEN_NAO_AUTORIZADO = 'Token inválido.'
+
+
+class MessagePrecoFormulario(Enum):
+    PRECO_PRECISA_SER_ENVIADO = 'O preço precisa ser enviado.'
